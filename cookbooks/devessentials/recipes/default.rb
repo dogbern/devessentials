039e7bf07871e2b25dd03030e8c8505b55cd94e9
dogbern <dogbern@gmail.com>
2019-08-13 00:49:09 -0500

    new repo

---
+++ cookbooks/devessentials/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: devessentials
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'http'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
